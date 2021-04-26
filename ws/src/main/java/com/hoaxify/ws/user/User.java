8abc37fc5beea7ea21770f6630166381fa11fc94
mozkan <m.ozkan0093@gmail.com>
2021-04-26 10:41:09 +0300

    first commit of WS side-SpringBoot

---
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -0,0 +1,21 @@
+package com.hoaxify.ws.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String username;
+	private String displayName;
+	private String password;
+	
+}
---
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -0,0 +1,21 @@
+package com.hoaxify.ws.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hoaxify.ws.shared.GenericResponse;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	UserService userService;
+	
+	@PostMapping("/api/1.0/users")
+	public GenericResponse createUser(@RequestBody User user) {
+		userService.save(user);
+		return new GenericResponse("user created");
+	}
+}
